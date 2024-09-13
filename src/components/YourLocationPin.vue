0ce1f78183a553a8c257b1014a70c80abf5dd426
Kim Fransson <kim.fransson@sigmatechnology.se>
2024-09-13 14:20:18 +0200

    Created vercel location function

--- src/components/YourLocationPin.vue
+++ src/components/YourLocationPin.vue
@@ -1,50 +1,44 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import LocalizationIcon from './icons/basic/LocalizationIcon.vue';
-// import { useGeolocation } from '@vueuse/core';
+import { useGeolocation } from '@vueuse/core';
 
-// const ERROR_TEXT = "We could not find your location";
+const ERROR_TEXT = "We could not find your location";
 
-// const { coords, error } = useGeolocation()
+const { coords, error } = useGeolocation()
 
-// const loading = ref(false)
-const locationText = ref('Stortorget 13B')
+const loading = ref(false)
+const locationText = ref('')
 
-// async function getLocation(latitude, longitude) {
-//     loading.value = true;
-//     try {
-//         const res = await fetch(`
-//             https://api.opencagedata.com/geocode/v1/json?q=${latitude}%2C${longitude}&key=...`);
-//         const data = (await res.json());
-//         const roadAddressCountry = data.results[0].formatted.split(',');
-//         if (roadAddressCountry[0] === 'unnamed road') {
-//             locationText.value = roadAddressCountry[1].trim();
-//         } else {
-//             locationText.value = roadAddressCountry[0].trim();
-//         }
-//     } catch (error) {
-//         locationText.value = ERROR_TEXT;
-//     } finally {
-//         loading.value = false;
-//         pause();
-//     }
-// }
+async function getLocation(latitude, longitude) {
+    loading.value = true;
+    try {
+        const res = await fetch(`/api/location?latitude=${latitude}&longitude=${longitude}`);
+        const data = (await res.json());
+        if (data.road) {
+            locationText.value = data.road;
+        } else {
+            locationText.value = data.address;
+        }
+    } catch (error) {
+        locationText.value = ERROR_TEXT;
+    } finally {
+        loading.value = false;
+    }
+}
 
-// watchEffect(() => {
-//     if (coords) {
-//         const { latitude, longitude } = coords.value;
-//         if (latitude !== Infinity && longitude !== Infinity) {
-//             // todo: Need to create a vercel function
-//             // getLocation(latitude, longitude)
-//         }
-//     }
-// })
+watchEffect(() => {
+    const { latitude, longitude } = coords.value;
+    if (latitude !== Infinity && longitude !== Infinity) {
+        getLocation(latitude, longitude)
+    }
+})
 
-// watchEffect(() => {
-//     if (error) {
-//         locationText.value = ERROR_TEXT
-//     }
-// })
+watchEffect(() => {
+    if (error) {
+        locationText.value = ERROR_TEXT
+    }
+})
 
 </script>
 
