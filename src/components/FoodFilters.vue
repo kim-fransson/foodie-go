e1aa2e1fd2aab7d41716b7582c55c0c30cac64a8
Kim Fransson <kim.fransson@sigmatechnology.se>
2024-09-18 15:41:48 +0200

    Made a simple carousel component

--- src/components/FoodFilters.vue
+++ src/components/FoodFilters.vue
@@ -1,11 +1,4 @@
 <script setup>
-// todo:
-//      * Scroll behavior for mobile?
-//      * Accessibility improvements for keyboard
-
-import { ref, watchEffect } from 'vue';
-import { useResizeObserver } from '@vueuse/core'
-
 import AsianIcon from './icons/food/AsianIcon.vue';
 import BreakfastIcon from './icons/food/BreakfastIcon.vue';
 import BurgerIcon from './icons/food/BurgerIcon.vue';
@@ -20,12 +13,10 @@ import PizzaIcon from './icons/food/PizzaIcon.vue';
 import SeafoodIcon from './icons/food/SeafoodIcon.vue';
 import SushiIcon from './icons/food/SushiIcon.vue';
 import VeganIcon from './icons/food/VeganIcon.vue';
-import ArrowIcon from './icons/basic/ArrowIcon.vue';
 import { useRestaurantPreferences } from '@/stores/restaurant-preferences';
+import HorizontalCarousel from './HorizontalCarousel.vue';
 
 const restaurantPreferences = useRestaurantPreferences();
-
-const TRANSLATE_AMOUNT = 200;
 
 const filters = [
     { value: "asian", icon: AsianIcon },
@@ -43,68 +34,19 @@ const filters = [
     { value: "sushi", icon: SushiIcon },
     { value: "vegan", icon: VeganIcon }
 ];
-
-const listRef = ref(null);
-const translateList = ref(0);
-const isLeftButtonVisible = ref(false);
-const isRightButtonVisible = ref(false);
-
-useResizeObserver(listRef, (entries) => {
-    const entry = entries[0]
-    const { clientWidth, scrollWidth } = entry.target;
-
-    isLeftButtonVisible.value = translateList.value > 0;
-    isRightButtonVisible.value = translateList.value + clientWidth < scrollWidth;
-})
-
-function translateListRight() {
-    const newTranslate = translateList.value + TRANSLATE_AMOUNT;
-    const edge = listRef.value.scrollWidth;
-    const width = listRef.value.clientWidth;
-    translateList.value = newTranslate + width >= edge ? edge - width : newTranslate;
-}
-
-function translateListLeft() {
-    const newTranslate = translateList.value - TRANSLATE_AMOUNT;
-    translateList.value = newTranslate <= 0 ? 0 : newTranslate;
-}
-
-watchEffect(() => {
-    if (listRef.value) {
-        isLeftButtonVisible.value = translateList.value > 0;
-        isRightButtonVisible.value = translateList.value + listRef.value.clientWidth < listRef.value.scrollWidth;
-    }
-})
-
 </script>
 
 <template>
-    <div ref="listRef" class="overflow-hidden pl-8 mt-4 relative">
-        <div v-if="isLeftButtonVisible" class="top-1/2 left-0 z-50 absolute -translate-y-1/2 flex items-center justify-center aspect-square h-full
-            bg-gradient-to-r from-base-100 to-transparent">
-            <button @click="translateListLeft" class="btn-circle btn btn-xs">
-                <ArrowIcon class="rotate-180 w-5 h-5" />
-            </button>
+    <HorizontalCarousel>
+        <div v-for="filter in filters" :key="filter.value">
+            <input class="appearance-none outline-none absolute peer" type="checkbox" name="food-filter"
+                :true-value="filter.value" false-value="" :id="filter.value" v-model="restaurantPreferences.foodType">
+            <label
+                class="inline-flex flex-col items-center cursor-pointer peer-checked:bg-primary transition-colors rounded-lg p-1"
+                :for="filter.value">
+                <component :is="filter.icon" />
+                <span class="capitalize text-sm font-medium whitespace-nowrap">{{ filter.value }}</span>
+            </label>
         </div>
-        <fieldset :style="{ transform: `translateX(-${translateList}px)` }"
-            class="flex gap-12 transition-transform duration-300">
-            <div v-for="filter in filters" :key="filter.value">
-                <input class="appearance-none outline-none absolute peer" type="checkbox" name="food-filter"
-                    :true-value="filter.value" false-value="" :id="filter.value"
-                    v-model="restaurantPreferences.foodType">
-                <label
-                    class="inline-flex flex-col items-center cursor-pointer peer-checked:bg-primary transition-colors rounded-lg p-1"
-                    :for="filter.value">
-                    <component :is="filter.icon" />
-                    <span class="capitalize text-sm font-medium whitespace-nowrap">{{ filter.value }}</span>
-                </label>
-            </div>
-        </fieldset>
-        <div v-if="isRightButtonVisible" class="top-1/2 right-0 z-50 absolute -translate-y-1/2 flex items-center justify-center aspect-square h-full
-            bg-gradient-to-l from-base-100 to-transparent">
-            <button @click="translateListRight" class="btn-circle btn btn-xs">
-                <ArrowIcon class="h-5 w-5" />
-            </button>
-        </div>
-    </div>
+    </HorizontalCarousel>
 </template>
---
+++ src/components/HorizontalCarousel.vue
@@ -0,0 +1,67 @@
+<script setup>
+// todo:
+//      * Scroll behavior for mobile?
+//      * Accessibility improvements for keyboard
+//      * Persistent translate (e.g when switching views back and forth )
+
+import { ref, watchEffect } from 'vue';
+import { useResizeObserver } from '@vueuse/core'
+
+import ArrowIcon from './icons/basic/ArrowIcon.vue';
+
+const TRANSLATE_AMOUNT = 200;
+
+const listRef = ref(null);
+const translateList = ref(0);
+const isLeftButtonVisible = ref(false);
+const isRightButtonVisible = ref(false);
+
+useResizeObserver(listRef, (entries) => {
+    const entry = entries[0]
+    const { clientWidth, scrollWidth } = entry.target;
+
+    isLeftButtonVisible.value = translateList.value > 0;
+    isRightButtonVisible.value = translateList.value + clientWidth < scrollWidth;
+})
+
+function translateListRight() {
+    const newTranslate = translateList.value + TRANSLATE_AMOUNT;
+    const edge = listRef.value.scrollWidth;
+    const width = listRef.value.clientWidth;
+    translateList.value = newTranslate + width >= edge ? edge - width : newTranslate;
+}
+
+function translateListLeft() {
+    const newTranslate = translateList.value - TRANSLATE_AMOUNT;
+    translateList.value = newTranslate <= 0 ? 0 : newTranslate;
+}
+
+watchEffect(() => {
+    if (listRef.value) {
+        isLeftButtonVisible.value = translateList.value > 0;
+        isRightButtonVisible.value = translateList.value + listRef.value.clientWidth < listRef.value.scrollWidth;
+    }
+})
+
+</script>
+
+<template>
+    <div ref="listRef" class="overflow-hidden pl-8 mt-4 relative">
+        <div v-if="isLeftButtonVisible" class="top-1/2 left-0 z-50 absolute -translate-y-1/2 flex items-center justify-center aspect-square h-full
+            bg-gradient-to-r from-base-100 to-transparent">
+            <button @click="translateListLeft" class="btn-circle btn btn-xs">
+                <ArrowIcon class="rotate-180 w-5 h-5" />
+            </button>
+        </div>
+        <fieldset :style="{ transform: `translateX(-${translateList}px)` }"
+            class="flex gap-12 transition-transform duration-300">
+            <slot></slot>
+        </fieldset>
+        <div v-if="isRightButtonVisible" class="top-1/2 right-0 z-50 absolute -translate-y-1/2 flex items-center justify-center aspect-square h-full
+            bg-gradient-to-l from-base-100 to-transparent">
+            <button @click="translateListRight" class="btn-circle btn btn-xs">
+                <ArrowIcon class="h-5 w-5" />
+            </button>
+        </div>
+    </div>
+</template>
