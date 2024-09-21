b35bdd7f039d4242a66403a163adf3ea73e26149
Kim Fransson <kim.fransson@sigmatechnology.se>
2024-09-21 22:03:07 +0200

    Checkout mobile (last card)

--- src/components/drawer/ShoppingCart.vue
+++ src/components/drawer/ShoppingCart.vue
@@ -2,6 +2,8 @@
 import { useDrawerStore } from '@/stores/drawer';
 import { useShoppingCartStore } from '@/stores/shopping-cart';
 import ShoppingCartIcon from '../icons/basic/ShoppingCartIcon.vue';
+import MinusIcon from '../icons/basic/MinusIcon.vue';
+import AddIcon from '../icons/basic/AddIcon.vue';
 
 
 const shoppingCart = useShoppingCartStore();
@@ -15,5 +17,55 @@ const drawer = useDrawerStore();
         <h2 class="card-title">Your cart is empty</h2>
         <p>Add items from a restaurant or store to start a new cart</p>
         <button @click="drawer.close()" class="btn btn-primary btn-block">Start shopping</button>
+    </div>
+
+    <div v-else class="flex flex-col gap-4 mt-8">
+        <div v-for="[restaurant, items] in shoppingCart.getCheckoutSummary.restaurantNameItemsMap" :key="restaurant">
+            <h2 class="card-title capitalize">
+                {{ restaurant }}
+            </h2>
+            <div class="mt-3">
+                <div v-for="(item, index) in items" :key="item.id" class="flex flex-col">
+                    <div class="grid grid-cols-2">
+                        <p class="font-medium">{{ item.amount }} {{ item.name }}</p>
+                        <span class="opacity-80 text-sm row-start-2">$ {{ item.price }}</span>
+
+                        <div class="join justify-self-end self-center row-span-2">
+                            <button @click="shoppingCart.removeItem(item.id)"
+                                class="btn btn-xs btn-neutral join-item rounded-l-full">
+                                <MinusIcon />
+                            </button>
+
+                            <input type="number" min="0" max="9" :value="item.amount"
+                                @change="(e) => shoppingCart.addItem(restaurant, item, +e.target.value)" class="outline-none 
+                                    focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral h-6 max-w-6 
+                                    bg-neutral text-neutral-content text-center" />
+
+                            <button @click="shoppingCart.addItem(restaurant, item)"
+                                :class="`btn btn-xs btn-neutral join-item ${item.amount === 0 ? 'rounded-full' : 'rounded-r-full'}`">
+                                <AddIcon />
+                            </button>
+                        </div>
+                    </div>
+                    <div v-if="index !== items.length - 1" class="divider my-0.5"></div>
+                </div>
+            </div>
+        </div>
+
+        <dl
+            class="grid grid-cols-2 gap-1 mt-4 [&>dt]:text-sm [&>dt]:opacity-80 [&>dd]:text-sm [&>dd]:font-semibold [&>dd]:text-right">
+            <dt>Subtotal</dt>
+            <dd>${{ shoppingCart.getCheckoutSummary.subtotal }}</dd>
+
+            <dt>Delivery costs</dt>
+            <dd v-if="shoppingCart.getCheckoutSummary.deliveryCost > 0">${{
+                shoppingCart.getCheckoutSummary.deliveryCost }}</dd>
+            <dd class="text-primary" v-else>Free</dd>
+
+            <dt>Total</dt>
+            <dd>${{ shoppingCart.getCheckoutSummary.total }}</dd>
+        </dl>
+
+        <button class="btn btn-primary btn-block">Go to checkout</button>
     </div>
 </template>
